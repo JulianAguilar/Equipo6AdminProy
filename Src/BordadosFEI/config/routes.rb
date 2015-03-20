be55addc4eec049540151092f61b5f75a988fb3b
Hector Villa <dnsdirm@hotmail.com>
2015-03-20 09:04:55 -0600

    agregado welcome index registro e inicio y salida de session de clientes realizado

--- Src/BordadosFEI/config/routes.rb
+++ Src/BordadosFEI/config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :clientes
+  get 'welcome/index'
+
+  devise_for :clientes, :controllers => {registrations: 'registrations'}
+	#devise_scope :clientes do
+	#				get '/clientes/sign_out' => 'devise/sessions#destroy'
+	#end
   resources :proveedors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- Src/BordadosFEI/db/schema.rb
+++ Src/BordadosFEI/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320132903) do
+ActiveRecord::Schema.define(version: 20150320133708) do
 
   create_table "clientes", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20150320132903) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "Nombre"
+    t.string   "Apellido_paterno"
+    t.string   "Apellido_materno"
+    t.string   "Direccion"
+    t.string   "Telefono"
   end
 
   add_index "clientes", ["email"], name: "index_clientes_on_email", unique: true, using: :btree
