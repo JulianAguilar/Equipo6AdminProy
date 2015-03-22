7683c58385e9acd83dfb33f5720c6cbf6a16f2a1
Hector Villa <dnsdirm@hotmail.com>
2015-03-22 13:13:11 -0600

    Agregado la gestion de empleados sin personalizar con devise cambie el idioma de devise

--- Src/BordadosFEI/config/routes.rb
+++ Src/BordadosFEI/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :empleados
   resources :proveedores
   get 'welcome/index'
 
--- Src/BordadosFEI/db/schema.rb
+++ Src/BordadosFEI/db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322160023) do
+ActiveRecord::Schema.define(version: 20150322182401) do
+
+  create_table "empleados", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "empleados", ["email"], name: "index_empleados_on_email", unique: true, using: :btree
+  add_index "empleados", ["reset_password_token"], name: "index_empleados_on_reset_password_token", unique: true, using: :btree
 
   create_table "proveedores", id: false, force: true do |t|
     t.string "RFC",       null: false
