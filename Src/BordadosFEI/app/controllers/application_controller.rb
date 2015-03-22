d2cdbf5dae82b828c773731a9c7ae99942092901
Hector Villa <dnsdirm@hotmail.com>
2015-03-22 02:09:15 -0600

    cambie el idioma y modifique proveedores para que se bloquee el rfc cuando se edita

--- Src/BordadosFEI/app/controllers/application_controller.rb
+++ Src/BordadosFEI/app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_action :set_locale
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	def set_locale
+		I18n.locale = 'es-MX'
+	end
 end
