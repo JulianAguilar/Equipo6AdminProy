67a75c8cfd06e43d90b742c679c09dc0c1d5cd5f
Hector Villa <dnsdirm@hotmail.com>
2015-03-23 11:44:03 -0600

    Corregido error de validacion de los formularios en cliente y empleado

--- Src/BordadosFEI/app/models/cliente.rb
+++ Src/BordadosFEI/app/models/cliente.rb
@@ -3,4 +3,6 @@ class Cliente < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	validates :Nombre, :Apellido_paterno, :Apellido_materno, :Direccion, :Telefono, presence: true
+	validates :Telefono, numericality: {only_integer: true}
 end
--- Src/BordadosFEI/app/models/empleado.rb
+++ Src/BordadosFEI/app/models/empleado.rb
@@ -3,4 +3,6 @@ class Empleado < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+	validates :Nombre, :Apellido_paterno, :Apellido_materno, :Direccion, :Telefono, :Puesto, presence: true
+	validates :Telefono, numericality: {only_integer: true}
 end
