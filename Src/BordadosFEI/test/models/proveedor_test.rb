c523cc610999ffc62cea3fe7634e74f46754f393
Hector <dnsdirm@hotmail.com>
2015-04-17 13:37:28 -0500

    agregado correcciones de proveedore

--- Src/BordadosFEI/test/models/proveedor_test.rb
+++ Src/BordadosFEI/test/models/proveedor_test.rb
@@ -1,14 +1,9 @@
 require 'test_helper'
 
 class ProveedorTest < ActiveSupport::TestCase
-
-	test "inserta proveeedor vacio" do # si se inserta un vacio se muestra un a falla si no no muestra nada
-		@proveedor = Proveedor.new
-		assert !@proveedor.save, "se inserto un proveedor vacio"
-	end
-
 	test "inserta proveedor correcto" do
-		proveedor :create, :proveedor => :correcto
-		assert proveedor.save,"se guarada el proveedor"
+				proveedor = Proveedor.new
+				proveedor = proveedores(:correcto)
+				assets proveedor.save "no se guarado un proveedor correcto"
 	end
 end
